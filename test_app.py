6d01cf8aefbd36a694f04868bd7e5dec58bfca16
Ben Stager <bstager@tulane.edu>
2024-05-10 23:38:44 -0400

    added user input function for finding dataframe by year

--- test_app.py
+++ test_app.py
@@ -13,27 +13,69 @@ from nba_api.stats.endpoints import leaguegamefinder
 # def app
 app = Flask(__name__)
 
+
 all_team_ids = [teams.teams[i][0] for i in range(len(teams.teams))]
 all_team_names = [teams.teams[i][5] for i in range(len(teams.teams))]
 team_id_dict = {i:j for i,j in zip(all_team_ids, all_team_names)}
+dict_all_games = {team:leaguegamefinder.LeagueGameFinder(team_id_nullable=id).get_data_frames()[0] for team, id in zip(all_team_names,all_team_ids)}
+all_games_df = pd.concat(list(dict_all_games.values()), axis=0)
+df = dict_all_games['Los Angeles Lakers'].head(5)
+
 
 gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=1610612739)
 games = gamefinder.get_data_frames()[0]
 
-#  make an array of dataframes for all games
-dict_all_games = {team:leaguegamefinder.LeagueGameFinder(team_id_nullable=id).get_data_frames()[0] for team, id in zip(all_team_names,all_team_ids)}
-df = dict_all_games['Los Angeles Lakers'].head(5)
+def find_games_by_year_api_test(year):
+    games_year = all_games_df[all_games_df['GAME_DATE'].str.contains(str(year))]
+    return games_year.head(5)
+
+def find_games_by_year(year):
+    games_year = all_games_df[all_games_df['GAME_DATE'].str.contains(str(year))]
+    return games_year
+
+def find_games_by_team(team_str):
+    games_team = all_games_df[all_games_df['TEAM_NAME'].str.contains(team_str)]
+    return games_team
+
+def find_games_by_matchup(team_1, team_2):
+    team_1 = team_1.lower()
+    team_2 = team_2.lower()
+    games_matchup = all_games_df[(all_games_df['MATCHUP'].str.contains(team_1.upper())) & (all_games_df['MATCHUP'].str.contains(team_2.upper()))]
+
+    return games_matchup
+
+"""
+def process_form():
+    # Get user input from the form
+    input_data = request.form.get('input_data')
+
+    # Call the function with user input
+    df = find_games_by_year_api_test(input_data)
+
+    # Process the DataFrame as needed (e.g., convert to HTML)
+    html_table = df.to_html()
+
+    # Return the processed data to the frontend
+    return render_template('result.html', html_table=html_table)
+"""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    message = "Hello, World!"
-    
-    # Handle form submission
     if request.method == 'POST':
-        user_input = request.form['user_input']
-        return render_template('index.html', message=message, table=df.to_html(index=False), user_input=user_input)
-    
-    return render_template('index.html', message=message, table=df.to_html(index=False))
+        # Get the input year from the form
+        input_year = request.form.get('input_data')
+
+        # Call the function to find games by year
+        df = find_games_by_year_api_test(input_year)
+
+        # Process the DataFrame to HTML
+        html_table = df.to_html()
+
+        # Render the result template with the HTML table
+        return render_template('result.html', html_table=html_table)
+    else:
+        # Render the index template for the initial page load
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
